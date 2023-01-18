65a0437cb96f1e252778575745f6ee326ebf65d2
Min Hankyul <denirokr22@gmail.com>
2023-01-18 14:53:04 +0900

    스프링 핵심 원리 기본편 공부내용: 새로운 할인 정책 개발 할인 정책 교체시 문제점 확인 -> OCP, DIP 원칙 위배

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,10 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // 구체 클래스에도 의존, 할인 정책 변경시 클라이언트 코드 변경 필요 -> DIP, OCP 원칙 위배
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
