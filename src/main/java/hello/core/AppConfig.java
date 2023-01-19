2f64aa627a75a549e4f90b5aa58264c6c4f27f59
Min Hankyul <denirokr22@gmail.com>
2023-01-19 12:08:02 +0900

    스프링 핵심 원리 기본편 공부내용: AppConfig 리팩터링하여 역할에 따른 구현 명시화 -> 구현체 변경 쉬움, 전체 구성 파악 빠름

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import hello.core.order.OrderServiceImpl;
 // 구동에 필요한 객체 생성하는 클래스
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자 주입
+        return new MemberServiceImpl(memberRepository()); // 생성자 주입
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
