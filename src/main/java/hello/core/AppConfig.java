3cdc20d7f1c1d3fa5a37fcc52e8eee0763f2480d
Min Hankyul <denirokr22@gmail.com>
2023-01-19 13:26:49 +0900

    스프링 핵심 원리 기본편 공부내용: 스프링 컨테이너 통한 의존성 주입 1. @Configuration 설정 정보로 인식 2. @Bean 모두 호출 반환된 객체 컨테이너에 저장 3. applicationContext.getBean(name: 메소드명, type: .class)로 스프링 빈 찾아 사용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 구동에 필요한 객체 생성하는 클래스
+@Configuration // 어플리케이션 설정 정보임을 나타냄
 public class AppConfig {
+
+    @Bean // 스프링 컨테이너에 등록
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
-    private static MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 }
