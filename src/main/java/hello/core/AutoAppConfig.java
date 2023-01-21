cc06d54defdb57e44de1056b7b4ec096ab24efbd
Min Hankyul <denirokr22@gmail.com>
2023-01-21 16:55:56 +0900

    스프링 핵심 원리 기본편 공부내용: 컴포넌트 스캔 필터(어노테이션, 타입 등) Exclude 간혹 사용하나 Include 거의 사용하지 않음 같은 이름 빈 등록시 충돌 발생 수동빈 등록시 같은 이름 자동빈(스프링: 자동빈을 오버라이딩 -> 스프링부트: 에러)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -14,5 +17,8 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
