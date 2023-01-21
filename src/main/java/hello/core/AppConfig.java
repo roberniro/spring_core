f4069635eecb23496f4d432e7cf2489acf847abd
Min Hankyul <denirokr22@gmail.com>
2023-01-21 15:14:04 +0900

    스프링 핵심 원리 기본편 공부내용: AppConfig 에서 싱글톤 지켜지는 원리 -> @Configuration 어노테이션 붙이면 바이트코드 조작하여 AppConfig 상속받은 임의의 클래스를 빈에 등록 빈에 등록된 클래스면 컨테이너에서 찾아 반환, 아니면 기존 로직 따라 객체 생성하고 등록

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -16,18 +16,39 @@ import org.springframework.context.annotation.Configuration;
 @Configuration // 어플리케이션 설정 정보임을 나타냄
 public class AppConfig {
 
+    /*
+    * @Bean memberService -> new MemoryMemberRepository()
+    * @Bean orderService -> new MemoryMemberRepository()
+    * 싱글톤이 깨지지 않을까? -> @Configuration
+    *
+    * 예측
+    * call AppConfig.memberService
+    * call AppConfig.memberRepository
+    * call AppConfig.memberRepository
+    * call AppConfig.orderService
+    * call AppConfig.memberRepository
+    *
+    * 실제
+    * call AppConfig.memberService
+    * call AppConfig.memberRepository
+    * call AppConfig.orderService
+    */
+
     @Bean // 스프링 컨테이너에 등록
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
