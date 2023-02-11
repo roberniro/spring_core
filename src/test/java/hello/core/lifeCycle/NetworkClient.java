0379e9d690fe6532538a0be8652e5437e65cf464
Min Hankyul <denirokr22@gmail.com>
2023-02-11 14:17:10 +0900

    스프링 핵심 원리 기본편 공부내용: 생명주기 콜백 방법: 어노테이션(@PostConstruct, @PreDestroy) 최신 스프링에서 권장, 스프링 비종속적(자바 표준 어노테이션) 외부 라이브러리에 적용 불가능 -> 외부라이브러리에는 @Bean(initMethod, destroyMethod)

--- src/test/java/hello/core/lifeCycle/NetworkClient.java
+++ src/test/java/hello/core/lifeCycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifeCycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -12,6 +15,7 @@ public class NetworkClient {
         this.url = url;
     }
 
+    // 서비스 시작시 호출
     public void connect() {
         System.out.println("connect = " + url);
     }
@@ -20,16 +24,19 @@ public class NetworkClient {
         System.out.println("call: " + url + " message = " + message);
     }
 
+    // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close:" + url);
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
