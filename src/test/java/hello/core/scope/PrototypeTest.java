8a8ecc608588e716cb5ef3b22e031b346cc97b54
Min Hankyul <denirokr22@gmail.com>
2023-02-11 15:57:30 +0900

    스프링 핵심 원리 기본편 공부내용: - 빈 스코프란: 빈이 존재할 수 있는 범위 - 종류  싱글톤: 기본, 컨테이너 시작과 종료까지 유지  프로토타입: 스프링 컨테이너가 프로토타입 빈 생성과 의존관계 주입, 초기화까지만 관여, 클라이언트에 반환, 이후에 관리 않음 -> 클라이언트에 관리 책임, 이후에 스프링 컨테이너에 같은 요청시 항상 새로운 프로토타입 빈 생성  웹관련: request, session, application

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,44 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        //        ac.close(); // 스프링 컨테이너는 프로토타입 빈 반환 후 관리하지 않음
+        prototypeBean1.destroy(); // 클라이언트가 직접 종료
+        prototypeBean2.destroy();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
