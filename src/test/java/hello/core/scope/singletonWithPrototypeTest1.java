14747da826a79cb565d394de71f8613a44187cd5
Min Hankyul <denirokr22@gmail.com>
2023-02-13 12:35:19 +0900

    스프링 핵심 원리 기본편 공부내용: 의존관계 조회(DL: Dependency Lookup): 프로토타입 빈 조회 이외에도 사용됨 (프로토타입 빈은 실제 사용은 매우 드뭄) 1. Logic()에서 ac.getBean() 사용 2. 스프링이 제공하는 DL 기능인 ObjectProvider, ObjectFactory 사용: 기능 단순, 라이브러비 필요 없음, 스프링 의존적 3. JSR-330 Provider 자바 표준 사용: 기능 단순, 라이브러리 필요, 다른 컨테이너에서도 사용

--- src/test/java/hello/core/scope/singletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/singletonWithPrototypeTest1.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -37,20 +39,20 @@ public class singletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성시점에 주입
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider; // 스프링 기능으로 DL
+        private Provider<PrototypeBean> prototypeBeanProvider; // 자바 표준으로 DL
 
         public int logic() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); // 스프링 기능으로 DL
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); // 자바 표준으로 DL
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
